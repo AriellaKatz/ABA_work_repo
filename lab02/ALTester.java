0f9c5201d7f375d0c038c3308640f61dd7e49113
AriellaKatz <akatz30@stuy.edu>
2021-12-12 00:15:55 -0500

    lab02

--- lab02/ALTester.java
+++ lab02/ALTester.java
@@ -1,3 +1,24 @@
+/*
+ABA - Ariella Katz, Alif Rahman, Brianna Tieu, Tom, Grippy, Pepe
+APCS
+Lab02 -- Well Al B. Sorted
+2021-12-10
+time spent: 0.5 hrs
+*/
+
+/*
+DISCO:
+0. ArrayList = ArrayList<Integer> will produce a warning saying that the code
+   uses unsafe or unchecked operations because the first ArrayList is not
+   necessarily an array of Integers. However, it's still allowed.
+1. If you get the above warning, if you want more details (like what
+   specifically caused the warning) you can enter javac -Xlint:unechecked
+   Class.java into the terminal. (However, since it's just a warning, no
+   further action is actually necessary.)
+QCC:
+0.
+*/
+
 import java.util.*;
 
 public class ALTester {
@@ -17,6 +38,10 @@ public class ALTester {
 		for (int i = 0; i < aList.size(); i++) {
 			aList.set(i, (int)(Math.random() * 100));
 		}
+	}
+
+	public static void main(String[] args) {
+		
 	}
 
 }
